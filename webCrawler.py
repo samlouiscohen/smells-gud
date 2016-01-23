5631908e543bcdb2a30c1d7dc8060a142a6ce5de
Sam Cohen <samsam489@gmail.com>
2016-01-23 03:22:40 -0500

    None Type handling

--- webCrawler.py
+++ webCrawler.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
+#/Users/samcohen/GitHub/WhatsCookin
+#/Users/samcohen/Desktop/Programming/WebApps/smellsGud
 
 def getTotalInfo(url):
 
@@ -12,8 +14,6 @@ def getTotalInfo(url):
 	soup = BeautifulSoup(plain_text,'lxml')
 
 
-
-
 	#---------------
 	numFoods = range(10) 
 	completeHalls = dict()
@@ -21,28 +21,87 @@ def getTotalInfo(url):
 	hallFoods = dict()
 
 
-
+	#Go into 'tbody' and get each 'row'
 	for tr in soup.findAll("tbody"):
 		for td in tr.findAll("td"):
-			foodName = td.find('div',class_ = "views-field-field-meal-images-fid")
-			attributes = td.find('div',class_ = "views-field-tid")
 
-			zz = attributes.find("div")
-			print('hello')
-			print(type(zz))
-
-			if(type(attributes)=="bs4.element.Tag"):
-				print('good')
-				print(zz)
-			else:
-				print('Nope')
-				print(zz)
+			#Get food names
+			foodGenInfo = td.find('a',class_ = "imagefield-field_meal_images")#"views-field-field-meal-images-fid"
+			
+			#Return from the current function if over total foods on page
+			if(foodGenInfo is None):
+				return
+			#print(foodGenInfo)
+			foodName = foodGenInfo.get('title')
+			print(foodName)
 
 
+			#Get food attributes
+			# attributes = td.find('div',class_ = "views-field-tid")
+			# if(attributes is not None):
 
-			if(type(attributes)=="bs4.element.Tag"):
-				for attrib in attributes.findAll("div"):
+			# 	print(attributes)
 
+
+			# if(foodGen is not None):
+			# 	print(type(foodGen))
+
+			# 	food2= foodGen.get('title')
+			# 	print(food2)
+			# else:
+			# 	print('fuck u meaty')
+
+				#x = foodGen.find('title')
+				#print(x)
+
+				# food2 = foodGen.get('title')
+
+				# print(food2)
+
+
+			#print(foodGen is None)
+			# if not(type(foodGen) == 'NoneType'):
+			# 	food2 = foodGen.find('a')
+			# 	food3 = food2.find('img',class_ = "title")
+			# 	print(food3)
+			
+			# attributes = td.find('div',class_ = "views-field-tid")
+			# print(type(foodName))
+			# #print(foodName.toString)
+			# x = foodName.get('title')
+			# print(x)
+			
+			# if not(foodName == 'None'):
+			# 	r = foodName.find('title')
+			# if( not(type(foodName)=='NoneType')):
+
+			# 	fudName = foodName.get("title")
+
+			#zz = attributes.findAll("div")
+			#print(foodName)
+			# print(fudName)
+			#print(zz)
+			#print("\n -----")
+
+			# #Control flow, to see if none type or not- so no errors
+			# if(type(attributes)=="<class 'bs4.element.Tag'>"):
+			# 	print('good')
+			# 	print(attributes.find("div"))
+
+
+			# else:
+			# 	print('Nope')
+			# 	print(type(attributes))
+			# 	#print(attributes.find("div"))
+
+
+
+			# if(type(attributes)=="bs4.element.Tag"):
+			# 	for attrib in attributes.findAll("div"):
+			# 		print('attrib is: ')
+			# 		print(attrib)
+			# else:
+			# 	print('Nope')
 
 
 
