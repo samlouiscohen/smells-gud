cb6d3b7187e12107a93e97060d2950371a914521
Leon Stilwell <leostilwell@gmail.com>
2016-01-24 15:17:24 -0500

    init_db

---
+++ init_db.py
@@ -0,0 +1,6 @@
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
